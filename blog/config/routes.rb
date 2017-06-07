7deb1ed13b9b2c2641e065588a0059eb467a7b0e
zahra abdallah <zkon360@gmail.com>
2017-06-07 15:48:31 -0400

    set up the application home page

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+root 'welcome#index'
 end
+#added root to this file this was created from this command bin/rails generate controller Welcome index
